69b3a64e9172aaf617cba65261af4a3ac8c28155
0DEStyle <109526123+0DEStyle@users.noreply.github.com>
2023-05-13 22:10:31 +0100

    Update bye1.cmd

--- FileDestroyer/bye1.cmd
+++ FileDestroyer/bye1.cmd
@@ -1,16 +1,18 @@
 @echo off
-rem //index is counter for file
+rem //index is the number counter for filename
 set /a Index=1
 
-rem //update the memory of index
+rem //EnableDelayedExpansion updates the memory of index
 setlocal EnableDelayedExpansion
 
 rem // check all files in current folder, erase all data inside the file
 rem // then rename the file name into number(index)
 rem // for whatever reason, can't run del after rename. 
 rem // moved del *.txt to next bat file.
+
+rem // you can add more extension below, format is *.WhateverExtension
 for %%a in (*.jpg *.jpeg *.png *.webp *.mp4 *.webm *.webp *.m4v *.gif) do ( 
-	echo xD type nul>%%a
+    type nul>%%a
     rename "*%%a" "!Index!.txt"
     set /a Index+=1
 )
